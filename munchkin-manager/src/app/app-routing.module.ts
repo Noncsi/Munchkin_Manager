d0888becdd2f5e9e92a61486ec00d708dc984436
Noemi Lengyel <lengyel.noemi96@gmail.com>
2020-04-02 14:22:29 +0200

    add basic pages, bootstrap

--- munchkin-manager/src/app/app-routing.module.ts
+++ munchkin-manager/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { AssembleComponent } from './components/assemble/assemble.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent },
+  { path: 'Assemble', component:AssembleComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- munchkin-manager/src/app/app.module.ts
+++ munchkin-manager/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AssembleComponent } from './components/assemble/assemble.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AssembleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
